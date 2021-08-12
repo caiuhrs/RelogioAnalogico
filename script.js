0c3971236ee14a63892dd18dbad19c1d9894c46d
Caio Henrique Reis Santos <caio.henriquereis@outlook.com>
2021-08-12 18:14:22 -0300

    Update script.js
    update

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
+var data = new Date();
+console.log(data);
+
+let hr = data.getHours();
+let min = data.getMinutes();
+let seg = data.getSeconds();
+console.log("Hora:" + hr + " Minuto: " + min + " segundo: " + seg);
+
 let posicaoHr = 20;
 let posicaoMin = 130;
 let posicaoSeg = 267;
