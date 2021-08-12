566e37fc1594cf544bec56bf806989d4b67d6357
Caio Henrique Reis Santos <caio.henriquereis@outlook.com>
2021-08-12 18:27:09 -0300

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
+function execultarRelógio(){
 var data = new Date();
 console.log(data);
 
@@ -10,10 +11,13 @@ let min = data.getMinutes();
 let seg = data.getSeconds();
 console.log("Hora:" + hr + " Minuto: " + min + " segundo: " + seg);
 
-let posicaoHr = 20;
-let posicaoMin = 130;
-let posicaoSeg = 267;
+let posicaoHr = (hr*360/12+(min*360/60)/12);;
+let posicaoMin = (min*360/60)+(seg*(360/60)/60);
+let posicaoSeg = seg*360/60;
 
 PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
 PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
 PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
+}
+
+var Intervalo = setInterval(execultarRelógio, 1000);
